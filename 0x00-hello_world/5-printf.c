e6ed733d1c6df43e8e5fda639d377556997bfd81
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-17 07:52:44 -0700

    C Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints a set of words with printf'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
