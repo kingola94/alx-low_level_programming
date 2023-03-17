dc2846231cd67be8846f279d5d5fb80805d11011
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-17 07:37:36 -0700

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this program uses put to output a code'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts('"Programming is like building a multilingual puzzle');
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
