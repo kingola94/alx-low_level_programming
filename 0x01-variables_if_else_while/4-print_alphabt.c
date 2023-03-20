097bd7bb7e3fe790bb2fb65165f175d24cec5df3
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-20 16:31:19 -0700

    C Programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (!(c == 'q' || c == 'e'))
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
