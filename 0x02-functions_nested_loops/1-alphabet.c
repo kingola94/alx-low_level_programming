c7e15ac66f4ba6c8bdc30bc24429a2347de98220
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-21 16:45:37 -0700

    C Programming

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - Entry point
+ *
+ * Description: 'prints all alphabets'
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
-	
+	char letter;
+
+	letter = 'a';
 	while (letter <= 'z')
 	{
 		_putchar(letter);
