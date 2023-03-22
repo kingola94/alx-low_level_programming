5008f68595c50236b843e4e8a7cd54074d34318c
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-22 14:21:18 -0700

    C Programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,33 @@
 
 void times_table(void)
 {
-	int i = 0, j = 0, p = 0;
+	int i = 0, j = 0, p;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		j = 0;
-		while (j <= 9)
+		for (j = 0; j <= 9; j++)
 		{
 			p = i * j;
 			if (j == 0)
 			{
-				_putchar(p + '0');
+				_putchar('0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
+			}
+			if (p < 10)
+			{
 				_putchar(' ');
+				_putchar(p + '0');
+			}
+			else
+			{
 				_putchar((p / 10) + '0');
 				_putchar((p % 10) + '0');
 			}
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
