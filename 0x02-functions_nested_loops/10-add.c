f9cbaaf8218c63d66a6c8524251a93e8a0a5e68b
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-22 16:17:58 -0700

    C Programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @num1: the character to be checked
+ * @num2: the character to be checked
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
