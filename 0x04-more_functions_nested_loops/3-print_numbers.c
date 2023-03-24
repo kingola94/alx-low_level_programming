e394446c4c878fe61d9813c4baeee1fb3651794f
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-23 21:30:06 -0700

    C Programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * Return: value of print_numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
