8c06dcdfe3023a5798fd7428678bae1c00a753d5
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-23 23:38:07 -0700

    C Programming

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ * @n: print _ n times
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
