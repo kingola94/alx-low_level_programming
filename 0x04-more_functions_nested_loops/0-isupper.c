10b75504a876dba783da19746c91056208c9f424
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-23 20:27:17 -0700

    C Programming

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check the code
+ * @c: prints if its uppercase
+ * Return: 1 if its upper and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
