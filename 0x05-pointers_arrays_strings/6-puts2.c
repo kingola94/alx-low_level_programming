b5c061e3d62fe54a09fe147573fab6c8226bc52d
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-30 14:18:59 -0700

    C Programming

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,15 +2,20 @@
 /**
  * puts2 - check code
  * @str: print half
- * Return: 0
+ * Return: void
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 ==0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
