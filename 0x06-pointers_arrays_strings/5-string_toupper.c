9e858e27577390638a7a9276164e10ebed08cacf
Samson Akindeinde <sakindeinde@gmail.com>
2023-03-30 17:33:33 -0700

    C Programming

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - check the code
+ * @str: changes all lowercase letters
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 'a' + 'A';
+		}
+		ptr++;
+	}
+	return (str);
+}
