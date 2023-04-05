2b0ae5da0fc8621e09be96a436d0d65331a2cedb
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-04 18:09:52 -0700

    C Programming

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+/**
+ * _strchr - check code
+ * @s: pointer s
+ * @c: pointer c
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int k;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		if (s[k] == c)
+		{
+			return (&s[k]);
+		}
+	}
+	if (c == '\0')
+	{
+		return &s[strlen(s)];
+	}
+	return (NULL);
+}
