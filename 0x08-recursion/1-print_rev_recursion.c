d6eedf16e74356c9641676f749b937201ba0cec5
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-06 14:39:49 -0700

    C Programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - check the code
+ * @s: print in reverse
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		return;
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
