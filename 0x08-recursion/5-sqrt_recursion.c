a758b4fd20e782c7a9f2cdc59947958ca04d8cd8
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-06 19:31:35 -0700

    C Programming

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,45 +6,29 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (1);
-	}
-	else
-	{
-		return (_sqrt(n, 1, n));
-	}
+	return (_sqrt(n, 1));
 }
+
 /**
  * _sqrt - check code
  * @n: int n
  * @a: int a
- * @b: int b
  * Return: 0
  */
-int _sqrt(int n, int a, int b)
+int _sqrt(int n, int a)
 {
-	int norm;
+	int norm = a * a;
 
-	if (a > b)
+	if (norm > n)
 	{
 		return (-1);
 	}
-	norm = (a + b) / 2;
-	if (norm * norm == n)
+	else if (norm == n)
 	{
-		return (norm);
-	}
-	else if (norm * norm > n)
-	{
-		return (_sqrt(n, a, norm - 1));
+		return (a);
 	}
 	else
 	{
-		return (_sqrt(n, norm + 1, b));
+		return (_sqrt(n, a + 1));
 	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,4 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int a, int b);
+int _sqrt(int n, int a);
