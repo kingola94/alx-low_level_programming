21371b1f6776661ef56502380f2b0716b59b1006
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-06 18:54:47 -0700

    C Programming

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - check the code
+ * @n: natural square root
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (_sqrt(n, 1, n));
+	}
+}
+/**
+ * _sqrt - check code
+ * @n: int n
+ * @a: int a
+ * @b: int b
+ * Return: 0
+ */
+int _sqrt(int n, int a, int b)
+{
+	int norm;
+
+	if (a > b)
+	{
+		return (-1);
+	}
+	norm = (a + b) / 2;
+	if (norm * norm == n)
+	{
+		return (norm);
+	}
+	else if (norm * norm > n)
+	{
+		return (_sqrt(n, a, norm - 1));
+	}
+	else
+	{
+		return (_sqrt(n, norm + 1, b));
+	}
+}
