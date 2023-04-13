a2b1bca4f1a0b785ed36070fc83cea9826ccb8aa
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-12 20:31:33 -0700

    C Programming

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	arr = (char*) malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+		_putchar(c);
+	}
+	return arr;
+}
