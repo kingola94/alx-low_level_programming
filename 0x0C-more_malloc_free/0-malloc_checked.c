581a6e8db3f2469807e3a7e3676c4ba25e71671e
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-14 20:27:31 -0700

    C Programming

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * malloc_checked - check code
+ * @b: malloc function
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+
+	if (!ptr)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
