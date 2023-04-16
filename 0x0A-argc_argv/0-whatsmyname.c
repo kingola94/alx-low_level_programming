82902f6ff6315668570916b68f3a6e153126dd3c
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-15 19:17:03 -0700

    C Programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments passed to the program
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
