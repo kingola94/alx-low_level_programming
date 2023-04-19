7ed4ab220417864da7ef06d6368b5312878bd42f
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-18 21:29:36 -0700

    C Programming

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - check code
+ * @d: Pointer to struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
