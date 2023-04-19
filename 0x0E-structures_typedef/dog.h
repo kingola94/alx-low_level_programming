b20825d31bd16805ec863d3e0ab03c79c6995655
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-18 21:16:11 -0700

    C Programming

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
