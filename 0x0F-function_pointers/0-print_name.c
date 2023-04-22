599b5a30cca38703d48404ddcb934f4ab25b0d6f
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-21 19:46:52 -0700

    C Programming

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - prints a name
  * @name: name to print
  * @f: function pointer to specify how to print the name
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
